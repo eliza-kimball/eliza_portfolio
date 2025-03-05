26ed54ed3dc8ef2cb6370d5134e7a8089557ebf6
eliza-kimball <156148008+eliza-kimball@users.noreply.github.com>
2025-03-05 13:50:59 -0700

    node, vite debugging

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+// Convert import.meta.url to a path
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const root = __dirname
+const outDir = resolve(__dirname, 'dist')
+
 export default defineConfig({
+  root,
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'), // Root index.html
+        advising: resolve(root, 'src/cs_advising.html'), // Corrected path
+        weather: resolve(root, 'src/weather.html'), // Corrected path
+      }
+    }
+  }
 })
